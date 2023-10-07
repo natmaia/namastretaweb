06ba15f336eafe60381c617993c6a45005c80266
Nathalia Maia <devmaia@outlook.com>
2023-10-07 00:50:29 -0300

    Obras Crud 🩷

--- src/actions/obras.js
+++ src/actions/obras.js
@@ -1,19 +1,20 @@
 "use server"
 
-export async function create(formData) {
-    //const url = "https://backend-namastreta-production.up.railway.app:8080/api/obra";
+import { revalidatePath } from "next/cache";
 
-    // função que deixa o json formatado
-    // console.log(Object.fromEntries(formData))
+// verificar o http do deploy, tive que fazer outra deploy
+const API_URL = "https://backend-namastreta-production.up.railway.app:8080/api/obra";
+
+export async function createObra(formData) {
     const options = {
         method: "POST",
         body: JSON.stringify(Object.fromEntries(formData)),
         headers: {
             "Content-Type": "application/json"
         }
-    }
+    };
 
-    const resp = await fetch(url, options);
+    const resp = await fetch(API_URL, options);
 
     if (resp.status !== 201) {
         const json = await resp.json();
@@ -21,11 +22,39 @@ export async function create(formData) {
             const mensagens = json.errors.reduce((str, erro) => str += ". " + erro.message, "");
             return { error: "Erro ao cadastrar" + mensagens };
         } else {
-            return { error: "Erro ao cadastrar"};
+            return { error: "Erro ao cadastrar" };
         }
     }
 
-    revalidatePath("/contas");
-    return { ok: "Conta cadastrada com sucesso" };
+    return { ok: "Obra cadastrada com sucesso" };
+}
+
+export async function getObraById(id) {
+    const resp = await fetch(`${API_URL}/${id}`);
+    const data = await resp.json();
+    return data;
+}
+
+export async function updateObra(id, formData) {
+    const options = {
+        method: "PUT",
+        body: JSON.stringify(Object.fromEntries(formData)),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    const resp = await fetch(`${API_URL}/${id}`, options);
+    const data = await resp.json();
+    return data;
+}
+
+export async function deleteObra(id) {
+    const options = {
+        method: "DELETE"
+    };
+
+    await fetch(`${API_URL}/${id}`, options);
+}
 
 }
