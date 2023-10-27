b3e8fd01fd51d9e00e45333f5eec52060fbd0523
JaelsonJonas <livedojonas@hotmail.com>
2023-10-27 12:48:40 -0300

    feat: update Obra

--- src/app/obra/page.js
+++ src/app/obra/page.js
@@ -1,31 +1,10 @@
-"use client"
-import { deleteObra, getObras } from "@/actions/obras";
+"use client";
+
 import Button from "@/components/Button";
-import Card from "@/components/Card";
 import NavBar from "@/components/NavBar";
-import { PlusCircleIcon } from '@heroicons/react/24/outline';
-import { useEffect, useState } from 'react';
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 function Obra() {
-  const [obras, setObras] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getObras();
-        if (Array.isArray(data)) {
-          setObras(data);
-        } else {
-          setObras([]);
-        }
-      } catch (error) {
-        console.error("Erro ao buscar obras:", error);
-      }
-    }
-
-    fetchData();
-  }, []);
-
   return (
     <>
       <NavBar active={"obra"} />
@@ -34,16 +13,17 @@ function Obra() {
         <div className="grid md:grid-cols-2 md:grid-rows-1 gap-2 m-4 space-x-96 justify-between items-center">
           <h2 className="text-green text-4xl"> Obras </h2>
 
-          <Button variant="primary" icon={<PlusCircleIcon className="h-6 w-6" />} href="/obras/new"> Criar Obra </Button>
+          <Button
+            variant="primary"
+            icon={<PlusCircleIcon className="h-6 w-6" />}
+            href="/obras/new"
+          >
+            {" "}
+            Criar Obra{" "}
+          </Button>
         </div>
 
-        <div className="grid gap-10 grid-cols-3">
-          {obras && obras.map(obra => {
-            return <Card key={obra.id} obra={obra} onDelete={() => handleDelete(obra.id)} />;
-          })}
-        </div>
-
-
+        <div className="grid gap-10 grid-cols-3"></div>
       </main>
     </>
   );
